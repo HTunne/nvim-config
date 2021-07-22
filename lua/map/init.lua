07301a3f9bd8d41fea8f2236dccfa5cd742aa8d1
HTunne <h.tunnicliffe@protonmail.com>
2021-07-22 13:46:56 +0100

    Add Telescope help_tags binding.

--- lua/map/init.lua
+++ lua/map/init.lua
@@ -42,6 +42,7 @@ wk.register({
     name = '+telescope',
     p = { require('telescope.builtin').git_files, 'find files in git repo' },
     f = { require('telescope.builtin').find_files, 'find files' },
+    h = { require('telescope.builtin').help_tags, 'find files' },
     g = { function() require('telescope.builtin').grep_string({ search = vim.fn.input("Grep for > ")}) end, 'grep over files' },
     w = { require('telescope.builtin').grep_string, 'find current word' },
   },
