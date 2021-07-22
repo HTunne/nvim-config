1622a08a03075d8beb5a2a9f8ba02523b71708e4
HTunne <h.tunnicliffe@protonmail.com>
2021-07-22 13:37:15 +0100

    Switch Paq format and install neorg. Switch to compe.

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -2,50 +2,55 @@
 --  paq  --
 -----------
 
-vim.cmd 'packadd paq-nvim'           -- load the package manager
-local paq = require('paq-nvim').paq  -- a convenient alias
-paq {'savq/paq-nvim', opt = true}    -- paq-nvim manages itself
--- Aesthetic
-paq {'HTunne/vim-one'}
-paq {'itchyny/lightline.vim'}
-paq {'RRethy/vim-hexokinase', run='make hexokinase'}
-paq {'kyazdani42/nvim-web-devicons'}
--- lsp
-paq {'neovim/nvim-lspconfig'}
--- Treesitter
-paq {'nvim-treesitter/nvim-treesitter'}
--- Completion
-paq {'nvim-lua/completion-nvim'}
-paq {'ervandew/supertab'}
-paq {'nvim-treesitter/completion-treesitter'}
---paq {'hrsh7th/nvim-compe'} -- Switch when regex transform is supported
---paq {'hrsh7th/vim-vsnip'}
---paq {'hrsh7th/vim-vsnip-integ'}
--- Telescope
-paq {'nvim-lua/popup.nvim'}
-paq {'nvim-lua/plenary.nvim'}
-paq {'nvim-telescope/telescope.nvim'}
-paq {'nvim-telescope/telescope-fzy-native.nvim'}
--- Snippets
-paq {'sirver/ultisnips'}
-paq {'htunne/vim-snippets'}
--- VimWiki
-paq {'vimwiki/vimwiki'}
-paq {'tools-life/taskwiki'}
---  Convenience
-paq {'kyazdani42/nvim-tree.lua'}
-paq {'liuchengxu/vim-which-key'}
-paq {'easymotion/vim-easymotion'}
-paq {'tpope/vim-commentary'}
-paq {'tpope/vim-surround'}
-paq {'tpope/vim-repeat'}
+require "paq" {
+  'savq/paq-nvim';    -- paq-nvim manages itself
+  -- Aesthetic
+  'HTunne/vim-one';
+  'itchyny/lightline.vim';
+  {'RRethy/vim-hexokinase', run='make hexokinase'};
+  'kyazdani42/nvim-web-devicons';
+  -- lsp
+  'neovim/nvim-lspconfig';
+  -- Treesitter
+  'nvim-treesitter/nvim-treesitter';
+  'nvim-treesitter/playground';
+  -- Completion
+  --paq {'nvim-lua/completion-nvim'}
+  --paq {'ervandew/supertab'}
+  --paq {'nvim-treesitter/completion-treesitter'}
+  'hrsh7th/nvim-compe'; -- Switch when regex transform is supported
+  'hrsh7th/vim-vsnip';
+  'hrsh7th/vim-vsnip-integ';
+  -- Telescope
+  'nvim-lua/popup.nvim';
+  'nvim-lua/plenary.nvim';
+  'nvim-telescope/telescope.nvim';
+  'nvim-telescope/telescope-fzy-native.nvim';
+  -- Snippets
+  'sirver/ultisnips';
+  'htunne/vim-snippets';
+  -- VimWiki
+  'vimwiki/vimwiki';
+  'tools-life/taskwiki';
+  --  Convenience
+  'kyazdani42/nvim-tree.lua';
+  'folke/which-key.nvim';
+  'easymotion/vim-easymotion';
+  'tpope/vim-commentary';
+  'tpope/vim-surround';
+  'tpope/vim-repeat';
 
-paq {'jupyter-vim/jupyter-vim'}
+  'jupyter-vim/jupyter-vim';
+
+  'vhyrro/neorg';
+  {'grafi-tt/lunajson', run='mv src lua'};
+}
 
 -- plugin config
 require('plugin.treesitter-config')
 require('plugin.nvim-tree-config')
-require('plugin.complete-config')
--- require('plugin.nvim-compe-config')
+--require('plugin.complete-config')
+require('plugin.nvim-compe-config')
 require('plugin.vimwiki-config')
 require('plugin.telescope-config')
+require('plugin.neorg-config')
