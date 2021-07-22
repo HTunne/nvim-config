3a750c6a46ceee44aa3962e11114dfe0d89310f7
HTunne <h.tunnicliffe@protonmail.com>
2021-07-22 13:40:12 +0100

    Add treesitter norg parser.

--- lua/plugin/treesitter-config.lua
+++ lua/plugin/treesitter-config.lua
@@ -1,10 +1,38 @@
 ------------------
 --  treesitter  --
 ------------------
+local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
+
+parser_configs.norg = {
+    install_info = {
+        url = "https://github.com/vhyrro/tree-sitter-norg",
+        files = { "src/parser.c" },
+        branch = "main"
+    },
+}
 
 local ts = require 'nvim-treesitter.configs'
 ts.setup {
-  ensure_installed = 'maintained',
+  ensure_installed = {
+    'bash',
+    'bibtex',
+    'c',
+    'cpp',
+    'css',
+    'html',
+    'javascript',
+    'json',
+    'latex',
+    'lua',
+    'python',
+    'regex',
+    'rust',
+    'scss',
+    'toml',
+    'typescript',
+    'vue',
+    'norg'
+  },
   highlight = {enable = true},
   indent = {enable = true},
 }
