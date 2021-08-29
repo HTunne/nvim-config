12ae64c249fdbdf95e0d2a925345c08d7080c946
HTunne <h.tunnicliffe@protonmail.com>
2021-08-29 15:19:15 +0100

    Add hop mappings.

---
+++ lua/map/hop.lua
@@ -0,0 +1,30 @@
+local hop = require('hop')
+local dir = require('hop.hint').HintDirection
+
+return {
+  name = '+hop',
+  w = {
+    function() hop.hint_words({direction=dir.AFTER_CURSOR}) end,
+    'hop word forward'
+  },
+  b = {
+    function() hop.hint_words({direction=dir.BEFORE_CURSOR}) end,
+    'hop word backward'
+  },
+  f = {
+    function() hop.hint_char1({direction=dir.AFTER_CURSOR}) end,
+    'hop char backward'
+  },
+  F = {
+    function() hop.hint_char1({direction=dir.BEFORE_CURSOR}) end,
+    'hop char backward'
+  },
+  j = {
+    function() hop.hint_lines_skip_whitespace({direction=dir.AFTER_CURSOR}) end,
+    'hop char down'
+  },
+  k = {
+    function() hop.hint_lines_skip_whitespace({direction=dir.BEFORE_CURSOR}) end,
+    'hop line up'
+  }
+}
