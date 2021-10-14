ae8437cc13d76b2d7faffd716637d23fc3f27284
HTunne <h.tunnicliffe@protonmail.com>
2021-10-14 16:44:56 +0100

    Update nvim-tree config to reflect changes to setup function.

--- lua/plugin/nvim-tree-config.lua
+++ lua/plugin/nvim-tree-config.lua
@@ -3,5 +3,8 @@
 -----------------
 
 vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
-vim.g.nvim_tree_auto_close = 0
 vim.g.nvim_tree_quit_on_open = 1
+
+require('nvim-tree').setup({
+  auto_close = false,
+})
