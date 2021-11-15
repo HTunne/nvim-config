b2f84fc5caed81c3fc8777216e6de6aebb699a1c
HTunne <h.tunnicliffe@protonmail.com>
2021-11-15 09:21:53 +0000

    Move tree ignore list to setup function.

--- lua/plugin/nvim-tree-config.lua
+++ lua/plugin/nvim-tree-config.lua
@@ -2,9 +2,9 @@
 --  nvim-tree  --
 -----------------
 
-vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
 vim.g.nvim_tree_quit_on_open = 1
 
 require('nvim-tree').setup({
   auto_close = false,
+  ignore_list = { '.git', 'node_modules', '.cache' }
 })
