1d1da4daaa852572618c13a167c35fe6c9220159
HTunne <htunnicliffe@protonmail.com>
2022-01-10 21:46:03 +0000

    Fix swapped adapter and config.

--- lua/dbg/adapters/nlua.lua
+++ lua/dbg/adapters/nlua.lua
@@ -1,12 +1,3 @@
-return {
-    {
-        type = "nlua",
-        request = "attach",
-        name = "Attach to running Neovim instance",
-        host = function() return "127.0.0.1" end,
-        port = function()
-            local val = 54231
-            return val
-        end
-    }
-}
+return function(callback, config)
+    callback({type = 'server', host = config.host, port = config.port})
+end
