f15add07e001e472785d7dda1c148d20bdbdb36d
HTunne <htunnicliffe@protonmail.com>
2022-04-14 13:34:44 +0100

    Add git signs code actions to null ls.

--- lua/plugin/null-ls-config.lua
+++ lua/plugin/null-ls-config.lua
@@ -5,13 +5,14 @@ local null_ls = require('null-ls')
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 require('null-ls').setup({
   sources = {
     formatting.stylua,
     formatting.black,
-    formatting.uncrustify,
     formatting.prettier,
     diagnostics.cppcheck,
+    code_actions.gitsigns,
   },
 })
