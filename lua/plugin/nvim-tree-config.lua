f21eea51dc86effda936e6b5c1981081eea5a96f
HTunne <htunnicliffe@protonmail.com>
2022-07-03 15:38:50 +0100

    Update nvim-tree config.

--- lua/plugin/nvim-tree-config.lua
+++ lua/plugin/nvim-tree-config.lua
@@ -3,7 +3,9 @@
 -----------------
 
 require('nvim-tree').setup({
-  nvim_tree_quit_on_open = true,
-  auto_close = false,
-  ignore_list = { '.git', 'node_modules', '.cache' },
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    },
+  },
 })
