06e856c342db40fd8ab0065794e468b419e505e5
HTunne <htunnicliffe@protonmail.com>
2023-01-18 23:05:44 +0000

    fix(lsp): Enforce offset encoding for null ls

--- lua/plugin/null-ls-config.lua
+++ lua/plugin/null-ls-config.lua
@@ -8,6 +8,9 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 require('null-ls').setup({
+  on_init = function(new_client, _)
+    new_client.offset_encoding = 'utf-32'
+  end,
   sources = {
     formatting.stylua,
     formatting.black,
