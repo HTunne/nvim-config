264dd83a8496f645334192ac06d1a1f1af0bf310
HTunne <h.tunnicliffe@protonmail.com>
2023-10-09 21:19:34 +0100

    style(comment): Run format

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,5 +8,9 @@ return {
   'lukas-reineke/indent-blankline.nvim',
   { 'norcalli/nvim-colorizer.lua', config = true },
   { 'akinsho/git-conflict.nvim', config = true, cond = require('util').is_git_repo },
-  { 'numToStr/Comment.nvim', config = true, keys = { { 'gc', mode = { 'n', 'v' } }, { 'gb', mode = { 'n', 'v' } } } },
+  {
+    'numToStr/Comment.nvim',
+    config = true,
+    keys = { { 'gc', mode = { 'n', 'v' } }, { 'gb', mode = { 'n', 'v' } } },
+  },
 }
