59e2071eb3eb2d0c421936e66e16689e8d0b063a
HTunne <h.tunnicliffe@protonmail.com>
2023-10-09 17:19:42 +0100

    fix(barbar): Remove global bar settings and move barbar maps to lazy keys

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,45 +1,57 @@
 return {
   'romgrk/barbar.nvim',
+  lazy = false,
   dependancies = {
     'nvim-tree/nvim-web-devicons',
+    'lewis6991/gitsigns.nvim',
   },
-  config = function()
-    vim.keymap.set('n', '<A-h>', '<cmd>BufferPrevious<cr>', { desc = 'goto previous buffer' })
-    vim.keymap.set('n', '<A-l>', '<cmd>BufferNext<cr>', { desc = 'goto next buffer' })
+  init = function()
+    vim.g.barbar_auto_setup = false
+  end,
+  opts = {
+    auto_hide = 1,
+  },
+  keys = {
+    { '<A-h>', '<cmd>BufferPrevious<cr>', desc = 'goto previous buffer' },
+    { '<A-left>', '<cmd>BufferPrevious<cr>', desc = 'goto previous buffer' },
+    { '<A-l>', '<cmd>BufferNext<cr>', desc = 'goto next buffer' },
+    { '<A-right>', '<cmd>BufferNext<cr>', desc = 'goto next buffer' },
 
     -- Re-order to previous/next
-    vim.keymap.set('n', '<A-H>', '<cmd>BufferMovePrevious<cr>', { desc = 'move buffer left' })
-    vim.keymap.set('n', '<A-L>', '<cmd>BufferMoveNext<cr>', { desc = 'move buffer right' })
+    { '<A-H>', '<cmd>BufferMovePrevious<cr>', desc = 'move buffer left' },
+    { '<A-S-left>', '<cmd>BufferMovePrevious<cr>', desc = 'move buffer left' },
+    { '<A-L>', '<cmd>BufferMoveNext<cr>', desc = 'move buffer right' },
+    { '<A-S-right>', '<cmd>BufferMoveNext<cr>', desc = 'move buffer right' },
 
     -- Goto buffer in position...
-    vim.keymap.set('n', '<A-1>', '<cmd>BufferGoto 1<cr>', { desc = 'goto buffer 1' })
-    vim.keymap.set('n', '<A-2>', '<cmd>BufferGoto 2<cr>', { desc = 'goto buffer 2' })
-    vim.keymap.set('n', '<A-3>', '<cmd>BufferGoto 3<cr>', { desc = 'goto buffer 3' })
-    vim.keymap.set('n', '<A-4>', '<cmd>BufferGoto 4<cr>', { desc = 'goto buffer 4' })
-    vim.keymap.set('n', '<A-5>', '<cmd>BufferGoto 5<cr>', { desc = 'goto buffer 5' })
-    vim.keymap.set('n', '<A-6>', '<cmd>BufferGoto 6<cr>', { desc = 'goto buffer 6' })
-    vim.keymap.set('n', '<A-7>', '<cmd>BufferGoto 7<cr>', { desc = 'goto buffer 7' })
-    vim.keymap.set('n', '<A-8>', '<cmd>BufferGoto 8<cr>', { desc = 'goto buffer 8' })
-    vim.keymap.set('n', '<A-9>', '<cmd>BufferGoto 9<cr>', { desc = 'goto buffer 9' })
-    vim.keymap.set('n', '<A-0>', '<cmd>BufferLast<cr>', { desc = 'goto last buffer' })
+    { '<A-1>', '<cmd>BufferGoto 1<cr>', desc = 'goto buffer 1' },
+    { '<A-2>', '<cmd>BufferGoto 2<cr>', desc = 'goto buffer 2' },
+    { '<A-3>', '<cmd>BufferGoto 3<cr>', desc = 'goto buffer 3' },
+    { '<A-4>', '<cmd>BufferGoto 4<cr>', desc = 'goto buffer 4' },
+    { '<A-5>', '<cmd>BufferGoto 5<cr>', desc = 'goto buffer 5' },
+    { '<A-6>', '<cmd>BufferGoto 6<cr>', desc = 'goto buffer 6' },
+    { '<A-7>', '<cmd>BufferGoto 7<cr>', desc = 'goto buffer 7' },
+    { '<A-8>', '<cmd>BufferGoto 8<cr>', desc = 'goto buffer 8' },
+    { '<A-9>', '<cmd>BufferGoto 9<cr>', desc = 'goto buffer 9' },
+    { '<A-0>', '<cmd>BufferLast<cr>', desc = 'goto last buffer' },
 
     -- Close buffer
-    vim.keymap.set('n', '<A-q>', '<cmd>BufferClose<cr>', { desc = 'close buffer' })
+    { '<A-q>', '<cmd>BufferClose<cr>', desc = 'close buffer' },
 
     -- Wipeout buffer
-    vim.keymap.set('n', '<A-w>', '<cmd>BufferWipeout<cr>', { desc = 'buffer wipeout' })
+    { '<A-w>', '<cmd>BufferWipeout<cr>', desc = 'buffer wipeout' },
 
     -- Close commands
-    vim.keymap.set('n', '<A-f>', '<cmd>BufferCloseAllButCurrent<cr>', { desc = 'close all but current buffer' })
-    vim.keymap.set('n', '<A->>', '<cmd>BufferCloseBuffersLeft<cr>', { desc = 'close buffers left' })
-    vim.keymap.set('n', '<A-<>', '<cmd>BufferCloseBuffersRight<cr>', { desc = 'close buffers right' })
+    { '<A-f>', '<cmd>BufferCloseAllButCurrent<cr>', desc = 'close all but current buffer' },
+    { '<A->>', '<cmd>BufferCloseBuffersLeft<cr>', desc = 'close buffers left' },
+    { '<A-<>', '<cmd>BufferCloseBuffersRight<cr>', desc = 'close buffers right' },
 
     -- Magic buffer-picking mode
-    vim.keymap.set('n', '<A-p>', '<cmd>BufferPick<cr>', { desc = 'pick buffer' })
+    { '<A-p>', '<cmd>BufferPick<cr>', desc = 'pick buffer' },
 
     -- Sort automatically by...
-    vim.keymap.set('n', '<leader>bb', '<cmd>BufferOrderByBufferNumber<cr>', { desc = 'sort buffers by number' })
-    vim.keymap.set('n', '<leader>bd', '<cmd>BufferOrderByDirectory<cr>', { desc = 'sort buffers by dir' })
-    vim.keymap.set('n', '<leader>bl', '<cmd>BufferOrderByLanguage<cr>', { desc = 'sort buffers by language' })
-  end,
+    { '<leader>bb', '<cmd>BufferOrderByBufferNumber<cr>', desc = 'sort buffers by number' },
+    { '<leader>bd', '<cmd>BufferOrderByDirectory<cr>', desc = 'sort buffers by dir' },
+    { '<leader>bl', '<cmd>BufferOrderByLanguage<cr>', desc = 'sort buffers by language' },
+  },
 }
--- lua/settings.lua
+++ lua/settings.lua
@@ -12,28 +12,28 @@ local function opt(scope, key, value)
 end
 
 local indent = 2
-opt('w', 'number', true) -- Print line number
+opt('w', 'number', true)         -- Print line number
 opt('w', 'relativenumber', true) -- Relative line numbers
-opt('w', 'list', true) -- Show some invisible characters (tabs...)
+opt('w', 'list', true)           -- Show some invisible characters (tabs...)
 opt('w', 'spell', true)
 --opt('w', 'signcolumn', true)
 opt('b', 'spelllang', 'en_gb')
-opt('b', 'expandtab', true) -- Use spaces instead of tabs
-opt('b', 'tabstop', indent) -- Number of spaces tabs count for
+opt('b', 'expandtab', true)    -- Use spaces instead of tabs
+opt('b', 'tabstop', indent)    -- Number of spaces tabs count for
 opt('b', 'shiftwidth', indent) -- Size of an indent
 opt('o', 'breakindent', true)
 -- opt('b', 'smartindent', true) -- Insert indents automatically
 opt('o', 'updatetime', 250)
 opt('o', 'background', 'dark')
 opt('o', 'completeopt', 'menuone,noselect') -- Completion options.
-opt('o', 'hidden', true) -- Enable modified buffers in background
-opt('o', 'joinspaces', false) -- No double spaces with join after a dot
-opt('o', 'scrolloff', 4) -- Lines of context
-opt('o', 'shiftround', true) -- Round indent
-opt('o', 'splitbelow', true) -- Put new windows below current
-opt('o', 'splitright', true) -- Put new windows right of current
-opt('o', 'termguicolors', true) -- True color support
-opt('o', 'wildmode', 'list:longest') -- Command-line completion mode
+opt('o', 'hidden', true)                    -- Enable modified buffers in background
+opt('o', 'joinspaces', false)               -- No double spaces with join after a dot
+opt('o', 'scrolloff', 4)                    -- Lines of context
+opt('o', 'shiftround', true)                -- Round indent
+opt('o', 'splitbelow', true)                -- Put new windows below current
+opt('o', 'splitright', true)                -- Put new windows right of current
+opt('o', 'termguicolors', true)             -- True color support
+opt('o', 'wildmode', 'list:longest')        -- Command-line completion mode
 opt('o', 'laststatus', 2)
 opt('o', 'showmode', false)
 opt('o', 'exrc', true)
@@ -43,7 +43,6 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.g.mapleader = ' '
 vim.g.tex_flavor = 'latex'
-vim.g.bufferline = { auto_hide = true }
 
 vim.api.nvim_exec(
   [[
