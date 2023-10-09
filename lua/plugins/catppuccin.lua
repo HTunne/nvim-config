bcd4048e59a0d1190ad4bf7cb71ae32cc75da530
HTunne <h.tunnicliffe@protonmail.com>
2023-10-09 17:26:59 +0100

    style(catppuccin): Run format

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -6,38 +6,38 @@ return {
     vim.cmd.colorscheme('catppuccin')
   end,
   opts = {
-  integrations = {
-    aerial = true,
-    barbar = true,
-    cmp = true,
-    dap = true,
-    gitsigns = true,
-    hop = true,
-    indent_blankline = {
-      enabled = true,
-      colored_indent_levels = false,
-    },
-    native_lsp = {
-      enabled = true,
-      virtual_text = {
-        errors = { 'italic' },
-        hints = { 'italic' },
-        warnings = { 'italic' },
-        information = { 'italic' },
+    integrations = {
+      aerial = true,
+      barbar = true,
+      cmp = true,
+      dap = true,
+      gitsigns = true,
+      hop = true,
+      indent_blankline = {
+        enabled = true,
+        colored_indent_levels = false,
       },
-      underlines = {
-        errors = { 'underline' },
-        hints = { 'underline' },
-        warnings = { 'underline' },
-        information = { 'underline' },
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { 'italic' },
+          hints = { 'italic' },
+          warnings = { 'italic' },
+          information = { 'italic' },
+        },
+        underlines = {
+          errors = { 'underline' },
+          hints = { 'underline' },
+          warnings = { 'underline' },
+          information = { 'underline' },
+        },
       },
+      nvimtree = true,
+      telescope = true,
+      treesitter = true,
+      lsp_trouble = true,
+      vimwiki = true,
+      which_key = true,
     },
-    nvimtree = true,
-    telescope = true,
-    treesitter = true,
-    lsp_trouble = true,
-    vimwiki = true,
-    which_key = true,
   },
-}
 }
