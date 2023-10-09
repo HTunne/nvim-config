91561508c5be39ee722bbe75d24eaac45fc4f445
HTunne <h.tunnicliffe@protonmail.com>
2023-10-09 17:46:38 +0100

    fix(dap): Ensure dap ui opens and closes at the correct times

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -3,6 +3,7 @@ return {
   dependancies = {
     'mfussenegger/nvim-dap',
   },
+  event = 'User NvimDapLoaded',
   keys = {
     {
       '<leader>du',
@@ -21,7 +22,7 @@ return {
     {
       '<leader>ds',
       function()
-        require('dapui').widgets.centered_float(widgets.scopes)
+        require('dapui').widgets.centered_float(require('dapui').widgets.scopes)
       end,
       desc = 'scopes',
     },
@@ -29,8 +30,15 @@ return {
   config = function()
     local dap = require('dap')
     local dapui = require('dapui')
+    dapui.setup()
     dap.listeners.after.event_initialized['dapui_config'] = function()
       dapui.open()
+    end
+    dap.listeners.before.event_terminated['dapui_config'] = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited['dapui_config'] = function()
+      dapui.close()
     end
   end,
 }
