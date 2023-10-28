6ea0181c254abece64e41d930361eccdfcecde20
HTunne <h.tunnicliffe@protonmail.com>
2023-10-28 13:10:13 +0100

    fix(twilight): Add 'User' to au group

--- lua/plugins/twilight.lua
+++ lua/plugins/twilight.lua
@@ -3,8 +3,8 @@ return {
   event = 'InsertEnter',
   config = function()
     require('twilight').setup()
-    vim.api.nvim_create_augroup('Twighlight', { clear = true })
-    vim.api.nvim_create_autocmd('InsertEnter', { group = 'Twighlight', command = 'TwilightEnable' })
-    vim.api.nvim_create_autocmd('InsertLeave', { group = 'Twighlight', command = 'TwilightDisable' })
+    vim.api.nvim_create_augroup('UserTwighlight', { clear = true })
+    vim.api.nvim_create_autocmd('InsertEnter', { group = 'UserTwighlight', command = 'TwilightEnable' })
+    vim.api.nvim_create_autocmd('InsertLeave', { group = 'UserTwighlight', command = 'TwilightDisable' })
   end,
 }
