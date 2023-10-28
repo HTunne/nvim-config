be6e9bdef792833edb0f1bbedeb04c7fb10fbd1e
HTunne <h.tunnicliffe@protonmail.com>
2023-10-28 13:00:26 +0100

    feat(oil): Add oil.nvim

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+  'stevearc/oil.nvim',
+  lazy = false,
+  opts = {},
+  keys = {
+    { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
+  },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+}
