86dfe28cdb17347e0ebac1426f379152f6989671
HTunne <h.tunnicliffe@protonmail.com>
2023-10-28 13:15:05 +0100

    fix(settings): Rewrite text yank in lua

--- lua/settings.lua
+++ lua/settings.lua
@@ -44,12 +44,9 @@ vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = ' '
 vim.g.tex_flavor = 'latex'
 
-vim.api.nvim_exec(
-  [[
-augroup highlight_yank
-    autocmd!
-    au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=400}
-augroup END
-]],
-  true
-)
+vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
+  group = vim.api.nvim_create_augroup('UserHighlightYank', {}),
+  callback = function()
+    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 400 })
+  end,
+})
