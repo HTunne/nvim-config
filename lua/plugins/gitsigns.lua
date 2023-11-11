8cf9fe197ac64fa4105e0649ab6b4f30f7fd8473
HTunne <h.tunnicliffe@protonmail.com>
2023-11-11 15:12:56 +0000

    fix(gitsigns): Set gitsigns keymaps in lazy key spec

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,56 +1,62 @@
 return {
   'lewis6991/gitsigns.nvim',
-  cond = require("util").is_git_repo,
-  opts = {
-    on_attach = function(bufnr)
-      local gs = package.loaded.gitsigns
-
-      local function map(mode, l, r, opts)
-        opts = opts or {}
-        opts.buffer = bufnr
-        vim.keymap.set(mode, l, r, opts)
-      end
-
-      -- Navigation
-      map('n', ']c', function()
+  cond = require('util').is_git_repo,
+  keys = {
+    -- Navigation
+    {
+      ']c',
+      function()
         if vim.wo.diff then
           return ']c'
         end
         vim.schedule(function()
-          gs.next_hunk()
+          require('gitsigns').next_hunk()
         end)
         return '<Ignore>'
-      end, { expr = true })
-
-      map('n', '[c', function()
+      end,
+      expr = true,
+    },
+    {
+      '[c',
+      function()
         if vim.wo.diff then
           return '[c'
         end
         vim.schedule(function()
-          gs.prev_hunk()
+          require('gitsigns').prev_hunk()
         end)
         return '<Ignore>'
-      end, { expr = true })
+      end,
+      expr = true,
+    },
 
-      -- Actions
-      map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>', { desc = 'stage hunk' })
-      map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>', { desc = 'reset hunk' })
-      map('n', '<leader>hS', gs.stage_buffer, { desc = 'stage buffer' })
-      map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-      map('n', '<leader>hR', gs.reset_buffer, { desc = 'reset buffer' })
-      map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview hunk' })
-      map('n', '<leader>hb', function()
-        gs.blame_line({ full = true })
-      end, { desc = 'blame line' })
-      map('n', '<leader>htb', gs.toggle_current_line_blame, { desc = 'toggle blame line' })
-      map('n', '<leader>hd', gs.diffthis, { desc = 'diff hunk' })
-      map('n', '<leader>hD', function()
-        gs.diffthis('~')
-      end, { desc = 'diff file' })
-      map('n', '<leader>htd', gs.toggle_deleted, { desc = 'toggle deleted' })
+    -- Actions
+    { '<leader>hs', require('gitsigns').stage_hunk, mode = { 'n', 'v' }, desc = 'stage hunk' },
+    { '<leader>hr', require('gitsigns').reset_hunk, mode = { 'n', 'v' }, desc = 'reset hunk' },
+    { '<leader>hS', require('gitsigns').stage_buffer, desc = 'stage buffer' },
+    { '<leader>hu', require('gitsigns').undo_stage_hunk, desc = 'undo stage hunk' },
+    { '<leader>hR', require('gitsigns').reset_buffer, desc = 'reset buffer' },
+    { '<leader>hp', require('gitsigns').preview_hunk, desc = 'preview hunk' },
+    {
+      '<leader>hb',
+      function()
+        require('gitsigns').blame_line({ full = true })
+      end,
+      desc = 'blame line',
+    },
+    { '<leader>htb', require('gitsigns').toggle_current_line_blame, desc = 'toggle blame line' },
+    { '<leader>hd', require('gitsigns').diffthis, desc = 'diff hunk' },
+    {
+      '<leader>hD',
+      function()
+        require('gitsigns').diffthis('~')
+      end,
+      desc = 'diff file',
+    },
+    { '<leader>htd', require('gitsigns').toggle_deleted, desc = 'toggle deleted' },
 
-      -- Text object
-      map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select hunk' })
-    end,
+    -- Text object
+    { 'ih', ':<C-U>Gitsigns select_hunk<CR>', mode = { 'o', 'x' }, desc = 'select hunk' },
   },
+  opts = {},
 }
