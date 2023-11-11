e1a5699c027d41b7e1631861d67a86573d622f92
HTunne <h.tunnicliffe@protonmail.com>
2023-11-11 16:30:30 +0000

    feat(trouble): Add trouble quickfix toggle keymap

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,8 +2,27 @@ return {
   'folke/trouble.nvim',
   config = true,
   keys = {
-    { '<leader>tt', '<cmd>TroubleToggle<CR>', 'trouble toggle' },
-    { '<leader>tr', '<cmd>TroubleRefresh<CR>', 'trouble refresh' },
+    {
+      '<leader>tt',
+      function()
+        require('trouble').toggle()
+      end,
+      desc = 'trouble toggle',
+    },
+    {
+      '<leader>tq',
+      function()
+        require('trouble').toggle('quickfix')
+      end,
+      desc = 'trouble toggle',
+    },
+    {
+      '<leader>tr',
+      function()
+        require('trouble').refresh()
+      end,
+      desc = 'trouble refresh',
+    },
   },
   dependencies = {
     'nvim-tree/nvim-web-devicons',
