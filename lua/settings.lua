9b59b849d9ac6e298f5981b28e6e3791c87db465
HTunne <h.tunnicliffe@protonmail.com>
2023-11-19 09:03:58 +0000

    refactor(settings): Change settings to use simpler vim.opt api

--- lua/settings.lua
+++ lua/settings.lua
@@ -2,42 +2,34 @@
 --  settings  --
 ----------------
 
-local scopes = { o = vim.o, b = vim.bo, w = vim.wo }
-
-local function opt(scope, key, value)
-  scopes[scope][key] = value
-  if scope ~= 'o' then
-    scopes['o'][key] = value
-  end
-end
-
 local indent = 2
-opt('w', 'number', true)         -- Print line number
-opt('w', 'relativenumber', true) -- Relative line numbers
-opt('w', 'list', true)           -- Show some invisible characters (tabs...)
-opt('w', 'spell', true)
---opt('w', 'signcolumn', true)
-opt('b', 'spelllang', 'en_gb')
-opt('b', 'expandtab', true)    -- Use spaces instead of tabs
-opt('b', 'tabstop', indent)    -- Number of spaces tabs count for
-opt('b', 'shiftwidth', indent) -- Size of an indent
-opt('o', 'breakindent', true)
--- opt('b', 'smartindent', true) -- Insert indents automatically
-opt('o', 'updatetime', 250)
-opt('o', 'background', 'dark')
-opt('o', 'completeopt', 'menuone,noselect') -- Completion options.
-opt('o', 'hidden', true)                    -- Enable modified buffers in background
-opt('o', 'joinspaces', false)               -- No double spaces with join after a dot
-opt('o', 'scrolloff', 4)                    -- Lines of context
-opt('o', 'shiftround', true)                -- Round indent
-opt('o', 'splitbelow', true)                -- Put new windows below current
-opt('o', 'splitright', true)                -- Put new windows right of current
-opt('o', 'termguicolors', true)             -- True color support
-opt('o', 'wildmode', 'list:longest')        -- Command-line completion mode
-opt('o', 'laststatus', 2)
-opt('o', 'showmode', false)
-opt('o', 'exrc', true)
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = 'yes'
+vim.opt.list = true
+vim.opt.spell = true
+vim.opt.spelllang = 'en_gb'
+vim.opt.expandtab = true
+vim.opt.tabstop = indent
+vim.opt.shiftwidth = indent
+vim.opt.breakindent = true
+vim.opt.smartindent = true
+vim.opt.updatetime = 250
+vim.opt.completeopt = { 'menuone', 'noselect' }
+vim.opt.hidden = true
+vim.opt.joinspaces = false
+vim.opt.scrolloff = 8 -- Lines of context
+vim.opt.shiftround = true -- Round indent
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
+vim.opt.termguicolors = true -- True color support
+vim.opt.wildmode = 'list:longest' -- Command-line completion mode
+vim.opt.laststatus = 2
+vim.opt.showmode = false
+vim.opt.exrc = true
+vim.opt.virtualedit = 'block'
 
+-- Disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
