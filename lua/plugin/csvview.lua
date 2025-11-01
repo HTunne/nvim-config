a6c40ce64ab859e198f81078a9a58b24c7ff898c
HTunne <hazzatun@gmail.com>
2025-11-01 21:11:40 +0000

    feat(csvview): Add keymaps for csv file navigation

--- lua/plugin/csvview.lua
+++ lua/plugin/csvview.lua
@@ -2,7 +2,17 @@ return {
   src = 'https://github.com/hat0uma/csvview.nvim',
   data = {
     after = function()
-      require('csvview').setup()
+      require('csvview').setup({
+        parser = { comments = { '#', '//' } },
+        keymaps = {
+          textobject_field_inner = { 'if', mode = { 'o', 'x' } },
+          textobject_field_outer = { 'af', mode = { 'o', 'x' } },
+          jump_next_field_end = { '<Tab>', mode = { 'n', 'v' } },
+          jump_prev_field_end = { '<S-Tab>', mode = { 'n', 'v' } },
+          jump_next_row = { '<Enter>', mode = { 'n', 'v' } },
+          jump_prev_row = { '<S-Enter>', mode = { 'n', 'v' } },
+        },
+      })
     end,
     keys = {
       { '<leader>v', '<CMD>CsvViewToggle<CR>', mode = { 'n' }, desc = 'toggle csvview' },
