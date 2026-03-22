b74e8a3f790d53c31ac4378ed87a1db4f3ff68d3
HTunne <hazzatun@gmail.com>
2026-03-22 09:46:24 +0000

    feat(init): Allow init from nix or pack installed plugins

--- init.lua
+++ init.lua
@@ -7,15 +7,23 @@ require('keymap')
 require('lsp')
 require('obsidian')
 
-vim.pack.add({ 'https://github.com/BirdeeHub/lze' }, { confirm = true })
 
 local plugins = {}
 
-for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath('config') .. '/lua/plugin', [[v:val =~ '\.lua$']])) do
+local config_directory = vim.fs.dirname(debug.getinfo(1, "S").source:sub(2))
+for _, file in ipairs(vim.fn.readdir(config_directory .. '/lua/plugin', [[v:val =~ '\.lua$']])) do
   table.insert(plugins, require('plugin.' .. file:gsub('%.lua$', '')))
 end
 
 local specs = {}
+if vim.g.skip_pack then
+  for _, p in pairs(plugins) do
+	  local spec = p.data or {}
+    spec.name = vim.fs.basename(p.src)
+	  table.insert(specs, spec)
+   end
+else
+vim.pack.add({ 'https://github.com/BirdeeHub/lze' }, { confirm = true })
 vim.pack.add(plugins, {
   load = function(p)
     local spec = p.spec.data or {}
@@ -24,5 +32,6 @@ vim.pack.add(plugins, {
   end,
   confirm = true,
 })
+end
 
 require('lze').load(specs)
