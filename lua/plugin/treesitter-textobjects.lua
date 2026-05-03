77036888cbac4a65059438ad623d3bf3229a56c1
Harry Tunnicliffe <harry@biotatrace.com>
2026-05-03 21:15:06 +0100

    feat(keymap): Add treesitter switch keymaps

--- lua/plugin/treesitter-textobjects.lua
+++ lua/plugin/treesitter-textobjects.lua
@@ -5,5 +5,13 @@ return {
     dep_of = {
       'mini.nvim',
     },
+    after = function()
+      vim.keymap.set('n', '<leader>a', function()
+        require('nvim-treesitter-textobjects.swap').swap_next('@parameter.inner')
+      end, { desc = 'Swap parameter next' })
+      vim.keymap.set('n', '<leader>A', function()
+        require('nvim-treesitter-textobjects.swap').swap_previous('@parameter.outer')
+      end, { desc = 'Swap parameter previous' })
+    end,
   },
 }
